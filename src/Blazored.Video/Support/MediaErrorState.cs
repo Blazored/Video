0167f189b038d9a6d7d17fe6a915c402ec19b95e
KellsoHP <KellsoHP@gmail.com>
2022-04-10 10:51:25 +0300

    fix tabs to spaces

--- src/Blazored.Video/Support/MediaErrorState.cs
+++ src/Blazored.Video/Support/MediaErrorState.cs
@@ -1,17 +1,17 @@
 ﻿namespace Blazored.Video.Support
 {
-    /// <summary>
-    /// Presents media error object.
-    /// </summary>
-    public class MediaErrorState
-    {
-        public MediaErrorType Code { get; set; }
+	/// <summary>
+	/// Presents media error object.
+	/// </summary>
+	public class MediaErrorState
+	{
+		public MediaErrorType Code { get; set; }
 
-        public string Message { get; set; }
+		public string Message { get; set; }
 
-        public override string ToString()
-        {
-            return this.Code.ToString();
-        }
-    }
+		public override string ToString()
+		{
+			return this.Code.ToString();
+		}
+	}
 }
